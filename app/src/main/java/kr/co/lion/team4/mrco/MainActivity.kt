921c57fadc0772a0c46fd6a0ca48c2d6e058f402
문태진 <mtj1015@naver.com>
2024-03-15 16:40:05 +0900

    Initial commit

--- app/src/main/java/kr/co/lion/team4/mrco/MainActivity.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 이 주석이 확인이 되나요? 되시면 제 브랜치로 답변을 주세요
     }
 }
