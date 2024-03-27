b2d282cb1b6145da49e1fcd910c0c1774a121d1a
songjuneki <dev.sjk0105@gmail.com>
2024-03-27 15:32:07 +0900

    feat: SplashScreen 라이브러리 추가 feat: 시작 화면(StartActivity) 추가 feat: 시작 화면 레이아웃 추가 feat: 회원가입 화면 레이아웃 추가

---
+++ app/src/main/java/kr/co/lion/team4/mrco/StartActivity.kt
@@ -0,0 +1,17 @@
+package kr.co.lion.team4.mrco
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+
+        setContentView(R.layout.activity_start)
+    }
+}
